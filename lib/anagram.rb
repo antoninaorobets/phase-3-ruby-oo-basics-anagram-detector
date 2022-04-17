ed80015ef45454aa9a2066b86d9a66919e2a95c5
antoninaorobets <pani.antonina.don@gmail.com>
2022-04-17 18:46:37 -0400

    commit tree d3ba48230064fc41ca5b8d16d19b7f3461296962

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+require "pry"
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(string_array)
+        result = []
+        # string_array = string.split(" ")
+        word_chars = @word.chars.sort
+        string_array.each do |check_this_word|
+            if check_this_word.chars.sort == word_chars
+                result << check_this_word
+            end
+        end
+        # binding.pry
+        result
+    end
+
+end
+#   binding.pry
+0
